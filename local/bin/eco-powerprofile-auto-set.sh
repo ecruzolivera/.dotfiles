516fc3e11967d87069c78ae87aa083c76a1aded2
Ernesto Cruz Olivera <ecruzolivera@pm.me>
2025-10-31 00:34:45 -0600

    remove the silent failure

--- local/bin/eco-powerprofile-auto-set.sh
+++ local/bin/eco-powerprofile-auto-set.sh
@@ -1,7 +1,7 @@
 if ls /sys/class/power_supply/BAT* &>/dev/null; then
   # This computer runs on a battery
-  powerprofilesctl set balanced || true
+  powerprofilesctl set balanced
 else
   # This computer runs on power outlet
-  powerprofilesctl set performance || true
+  powerprofilesctl set performance
 fi
